b7deff8e18cdf7ad20c0e4744374c620979699de
brunoBMonteiro <brunobilheri@hotmail.com>
2022-05-07 19:15:04 -0300

    Initialing project

---
+++ src/main/java/com/client/clientwebflux/ClientwebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.client.clientwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientwebfluxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/client/clientwebflux/ClientwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.clientwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
