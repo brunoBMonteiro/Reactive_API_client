9b582cb3d3ee1829f09a7dee56190405a28cee23
brunoBMonteiro <brunobilheri@hotmail.com>
2022-05-12 10:56:51 -0300

    Add unit tests on service

--- src/test/java/com/client/clientwebflux/service/ClientServiceTest.java
+++ src/test/java/com/client/clientwebflux/service/ClientServiceTest.java
@@ -3,19 +3,21 @@ package com.client.clientwebflux.service;
 import com.client.clientwebflux.domain.Client;
 import com.client.clientwebflux.repository.ClientRepository;
 import com.client.clientwebflux.stub.ClientCreator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @ExtendWith(SpringExtension.class)
-public class ClientServiceTest {
+class ClientServiceTest {
 
     @InjectMocks
     private ClientService clientService;
@@ -25,18 +27,67 @@ public class ClientServiceTest {
 
     private final Client client = ClientCreator.createValidClient();
 
-    @BeforeEach
-    public void setUp(){
-        Mockito.when(clientRepositoryMock.findAll())
-                .thenReturn(Flux.just(client));
-    }
 
     @Test
-    @DisplayName("FindAll retorna um flux de clientes")
-    public void findAllReturnFluxOfClientWhenSuccessful(){
+    @DisplayName("FindAll, retorna um flux de clientes")
+    void findAllReturnFluxOfClientWhenSuccessful(){
+        Mockito.when(clientRepositoryMock.findAll())
+                .thenReturn(Flux.just(client));
+
         StepVerifier.create(clientService.findAll())
                 .expectSubscription()
                 .expectNext(client)
                 .verifyComplete();
     }
+
+    @Test
+    @DisplayName("FindAll, retorna um flux de clientes")
+    void findAllReturnFluxClientErrorWhenFluxIsEmpty(){
+        Mockito.when(clientRepositoryMock.findAll())
+                .thenReturn(Flux.empty());
+
+        StepVerifier.create(clientService.findAll())
+                .expectSubscription()
+                .expectError(ResponseStatusException.class);
+    }
+
+    @Test
+    @DisplayName("FindById, retorna um mono com cliente quando encontrar id")
+    void findByIdReturnMonoClientWhenSuccessful(){
+        Mockito.when(clientRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.just(client));
+
+        StepVerifier.create(clientService.findById(1))
+                .expectSubscription()
+                .expectNext(client)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("FindById, retorna um Mono erro quando o cliente não existe")
+    void findByIdReturnMonoErrorWhenEmptyMonoIsReturned(){
+        Mockito.when(clientRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.empty());
+
+        StepVerifier.create(clientService.findById(1))
+                .expectSubscription()
+                .expectError(ResponseStatusException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Save client, salva client criado quando der sucesso!")
+    void saveCreateClientWhenSuccessful(){
+        Client clientToBeSaved = ClientCreator.createClientToBeSaved();
+
+        Mockito.when(clientRepositoryMock.save(ClientCreator.createClientToBeSaved()))
+                .thenReturn(Mono.just(client));
+
+
+        StepVerifier.create(clientService.createClient(clientToBeSaved))
+                .expectSubscription()
+                .expectNext(client)
+                .verifyComplete();
+    }
+
 }
